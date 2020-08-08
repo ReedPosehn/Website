97c4cb3a421a77b2f70fb361c48efac24a249f47
unknown <reedpo@hotmail.ca>
2020-08-08 00:12:51 -0600

    Simplifying site for now. Basic finished product at this stage.

--- src/App.js
+++ src/App.js
@@ -13,59 +13,15 @@ class App extends Component {
     this.state = {
       name: ''
     };
-    //this.handleChange = this.handleChange.bind(this);
-    //this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  /*handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => this.setState(state));
-  }
-  
-  recentFluc(event) {
-	  this.setState
-  }*/
-  
   render() {
 	  return (
 		<div className="App">
 			<header className="App-header">
 			<Navigation />
 			<Main />
-			{/*<form onSubmit={this.handleSubmit}>
-				<label htmlFor="name">Enter your name: </label>
-				<label htmlFor="name">Enter your name: </label>
-				<input
-				  id="name"
-				  type="text"
-				  value={this.state.name}
-				  onChange={this.handleChange}
-				/>
-				<button type="submit">Submit</button>
-			  </form>
-			  <p>{this.state.greeting}</p>*/}
 			 </header>
-			 {/*<div className="Main">
-			 <img src={photo} className="myphoto" />
-			 <img src={logo} className="App-logo" alt="logo" />
-			<p>
-				In testing!
-			</p>
-			<a
-			  className="App-link"
-			  href="https://reactjs.org"
-			  target="_blank"
-			  rel="noopener noreferrer"
-			>
-			  Learn React
-			</a>
-			 </div>*/}
 			<div className="footer">
 			<div className="footerText">
 			<p style={{margin: "0px", fontSize: 15}}>© 2020 Reed Posehn.</p>
@@ -81,7 +37,6 @@ const Navigation = () => (
 				<nav>
 					<ul>
 						<li><NavLink to='/'>Home</NavLink></li>
-						<li><NavLink to='/recentFluc'>Recent Fluctuations</NavLink></li>
 					</ul>
 				</nav>
 			);
@@ -90,7 +45,6 @@ const Navigation = () => (
 const Main = () => (
 				<Switch>
 					<Route exact path='/' component={Home}></Route>
-					<Route exact path='/recentFluc' component={RecentFluctuations}></Route>
 				</Switch>
 			);
 
@@ -104,7 +58,6 @@ class Home extends Component {
 					<div className="row">
 					<div className="leftColumn">
 						<img src={photo} className="myphoto" />
-						{/*<img src={logo} className="App-logo" alt="logo" />*/}
 						<p className="links">Links to Github and LinkedIn</p>	 
 						<div className="logos">
 						<BothLogos />
@@ -119,10 +72,7 @@ class Home extends Component {
 					<p style={{marginTop: "50px", marginBottom: "1px", color: "whitesmoke", fontSize: 30}}>Clients</p>
 					<p style={{margin: "1px", color: "whitesmoke", fontSize: 25}}>Nutrien - EtQ Developer (Desktop/Mobile Application)</p>
 					</div>
-					{/*<div className="midColumn">
-					<img src={github} className="githubLogo" />
-					<img src={linkedIn} className="linkedInLogo" />
-					</div>*/}
+
 					<div className="rightColumn">
 						<img src={birdsNest} className="birdsNest" />
 						<div className="rightColBox">
@@ -149,7 +99,7 @@ class Home extends Component {
 						</div>
 						<div className="rightColBox">
 							<div className="colBox">
-							<p style={{marginTop: "1px", fontSize: 15, color: "black"}}>Flight Simulator<br/>Chat Room<br/>Shell<br/>Kernal alterations<br/>Web Crawler<br/>Threading<br/>Basic AI Implementation<br/>
+							<p style={{marginTop: "1px", fontSize: 15, color: "black"}}>Flight Simulator<br/>Chat Room<br/>Shell<br/>Kernal alterations<br/>Web Crawler<br/>Threading<br/>Intro AI Programs<br/>
 							Web Services</p>
 							</div>
 							<div className="colBox">
@@ -157,58 +107,10 @@ class Home extends Component {
 							Operating Systems<br/>Databases<br/>Software Engineering<br/>Artificial Intelligence<br/>Web Programming<br/>GUIs<br/>Mobile and Cloud</p>
 							</div>
 						</div>
-					{/*<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-					>
-					Learn React
-					</a>*/}
 					</div>
 					</div>
 				</div>
 			</div>
-			</div>
-		);
-	}
-}
-			
-class RecentFluctuations extends Component {
-	render () {
-		return (
-			<div className='recentFluc'>
-				<h1 className='headerImage'>Recent Highs and Lows</h1>
-				<div className='stockTable'>
-					<div className='stockRow'>
-						<div className="stockColumn">
-							<p>test</p>
-						</div>
-						<div className="stockColumn">
-							<p>test</p>
-						</div>
-						<div className="stockColumn">
-							<p>test</p>
-						</div>
-						<div className="stockColumn">
-							<p>test</p>
-						</div>
-					</div>
-					<div className='stockRow'>
-						<div className="stockColumn">
-							<p>test</p>
-						</div>
-						<div className="stockColumn">
-							<p>test</p>
-						</div>
-						<div className="stockColumn">
-							<p>test</p>
-						</div>
-						<div className="stockColumn">
-							<p>test</p>
-						</div>
-					</div>
-				</div>
 			</div>
 		);
 	}
